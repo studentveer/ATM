640654e102f6fcd4fa8c6eafe50fa0f9976e8a26
studentveer <73462172+studentveer@users.noreply.github.com>
2021-04-17 14:21:11 +0530

    Add files via upload

---
+++ atm.py
@@ -0,0 +1,15 @@
+class Atm(object):
+    def __init__(self,CashWithdrawl, BalanceEnquiry):
+        self.CashWithdrawl = modal
+        self.color = color
+        
+
+    def CashWithdrawl(self):
+        print("CashWithdrawled")
+
+    def BalanceEnquiry(self):
+        print("10000 rs left")
+
+cash = Atm("Withdrawled","10000 rs left")
+print(cash.CashWithdrawl)
+cash.BalanceEnquiry()
